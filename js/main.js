0183725bb8c6375b847bc9dc59c56143bbb5dfbe
Elias Xool <elias.xool.96@gmail.com>
2023-09-10 14:45:38 -0600

    Adding functionality show buttons pokemon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const listaPokemon = document.querySelector("#listaPokemon");
+const buttonsHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for (let i = 1; i <= 150; i++) {
@@ -9,24 +10,29 @@ for (let i = 1; i <= 150; i++) {
 
 function mostrarPokemon(poke) {
     let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
-    console.log(tipos)
+    tipos = tipos.join('');
 
+    let pokeId = poke.id.toString();
+    if (pokeId.length === 1) {
+        pokeId = "00" + pokeId;
+    } else if (pokeId.length === 2) {
+        pokeId = "0" + pokeId;
+    }
 
     const div = document.createElement("div");
     div.classList.add("pokemon")
     div.innerHTML = `
-        <p class="pokemon-id-back">#${poke.id}</p>
+        <p class="pokemon-id-back">#${pokeId}</p>
         <div class="pokemon-imagen">
             <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}]">
         </div>
         <div class="pokemon-info">
             <div class="nombre-contenedor">
-                <p class="pokemon-id">#${poke.id}</p>
+                <p class="pokemon-id">#${pokeId}</p>
                 <h2 class="pokemon-nombre">${poke.name}</h2>
             </div>
             <div class="pokemon-tipos">
-                <p class="electric tipo">ELECTRIC</p>
-                <p class="fighting tipo">FIGHT</p>
+                ${tipos}
             </div>
             <div class="pokemon-stats">
                 <p class="stat">${poke.height}m</p>
@@ -37,25 +43,25 @@ function mostrarPokemon(poke) {
     listaPokemon.append(div);
 }
 
-/* 
-<div class="pokemon">
-    <p class="pokemon-id-back">#025</p>
-    <div class="pokemon-imagen">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu">
-    </div>
-    <div class="pokemon-info">
-        <div class="nombre-contenedor">
-            <p class="pokemon-id">#025</p>
-            <h2 class="pokemon-nombre">Pikachu</h2>
-        </div>
-        <div class="pokemon-tipos">
-            <p class="electric tipo">ELECTRIC</p>
-            <p class="fighting tipo">FIGHT</p>
-        </div>
-        <div class="pokemon-stats">
-            <p class="stat">4m</p>
-            <p class="stat">60kg</p>
-        </div>
-    </div>
-</div>
-*/
+buttonsHeader.forEach(button => button.addEventListener("click", (event) => {
+    const buttonID = event.currentTarget.id;
+
+    listaPokemon.innerHTML = "";
+
+    for (let i = 1; i <= 150; i++) {
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then((data) => {
+
+                if(buttonID === "ver-todos") {
+                    mostrarPokemon(data);
+                } else {
+                    const tipos = data.types.map(type => type.type.name);
+                    if (tipos.some(tipo => tipo.includes(buttonID))) {
+                        mostrarPokemon(data);
+                    }
+                }
+            });
+    }
+
+}))
